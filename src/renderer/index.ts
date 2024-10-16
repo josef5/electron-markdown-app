7271f852440bcc96346b2fce606e58abb3424a1f
Jose Espejo <joseespejo@yahoo.com>
2024-10-16 20:46:00 +0100

    refactor: add listeners for menu buttons

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,7 +1,37 @@
-import { renderMarkdown } from './markdown';
-import Elements from './elements';
+import { renderMarkdown } from "./markdown";
+import Elements from "./elements";
 
-Elements.MarkdownView.addEventListener('input', async () => {
+Elements.MarkdownView.addEventListener("input", async () => {
   const markdown = Elements.MarkdownView.value;
+
   renderMarkdown(markdown);
+
+  const hasChanges = await window.api.checkForUnsavedChanges(markdown);
+  Elements.SaveMarkdownButton.disabled = !hasChanges;
+
+  await window.api.checkForUnsavedChanges(markdown);
+});
+
+Elements.OpenFileButton.addEventListener("click", () => {
+  window.api.showOpenDialog();
+});
+
+Elements.ExportHtmlButton.addEventListener("click", () => {
+  const html = Elements.RenderedView.innerHTML;
+
+  window.api.showExportHtmlDialog(html);
+});
+
+Elements.SaveMarkdownButton.addEventListener("click", async () => {
+  const markdown = Elements.MarkdownView.value;
+
+  window.api.saveFile(markdown);
+});
+
+Elements.ShowFileButton.addEventListener("click", () => {
+  window.api.showInFolder();
+});
+
+Elements.OpenInDefaultApplicationButton.addEventListener("click", () => {
+  window.api.openInDefaultApplication();
 });
