9b2e706765de4286e610072d21635439eb35c309
Jose Espejo <joseespejo@yahoo.com>
2024-10-16 20:42:07 +0100

    typescript: declare method types

--- src/electron.d.ts
+++ src/electron.d.ts
@@ -3,3 +3,14 @@
 
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
 declare const MAIN_WINDOW_VITE_NAME: string;
+
+declare interface Window {
+  api: {
+    showOpenDialog: () => void;
+    showExportHtmlDialog: (html: string) => void;
+    saveFile: (content: string) => void;
+    checkForUnsavedChanges: (content: string) => Promise<boolean>;
+    showInFolder: () => void;
+    openInDefaultApplication: () => void;
+  };
+}
